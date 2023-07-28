c0deff774c4fbb077bb25112b78a92e263c461d2
Gabor Gulyas <guyko81@gmail.com>
2023-07-28 13:52:35 +0100

    rename package

--- setup.py
+++ setup.py
@@ -5,6 +5,6 @@ setup(
     version='0.1',
     description='A Python package to dynamically generate module from local Python file.',
     author='Gabor Gulyas',
-    url='https://github.com/guyko81/import_local_module',
+    url='https://github.com/guyko81/modgen',
     packages=find_packages(),
 )
