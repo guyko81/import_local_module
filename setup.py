7891c514bfe6e864959c69d981b111934fbad319
Gabor Gulyas <guyko81@gmail.com>
2023-07-28 13:55:54 +0100

    changed version and git address

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='modgen',
-    version='0.1',
+    version='0.2',
     description='A Python package to dynamically generate module from local Python file.',
     author='Gabor Gulyas',
     url='https://github.com/guyko81/modgen',
